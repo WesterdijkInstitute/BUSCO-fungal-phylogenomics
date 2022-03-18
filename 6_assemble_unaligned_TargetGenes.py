677811470c736d4b6f5c528b96fd858074cb6649
Jorge <jorge.c.navarro.munoz@gmail.com>
2022-03-18 17:22:09 +0100

    Step 6: increased compatibility in case results are not zipped

--- 6_assemble_unaligned_TargetGenes.py
+++ 6_assemble_unaligned_TargetGenes.py
@@ -22,16 +22,16 @@ def parameters_parser():
     parser.add_argument("-r", "--results", help="Base path with BUSCO results", \
         required=True, type=Path)
     parser.add_argument("-a", "--assemblies", help="List of assemblies from \
-        which to extract the sequences", \
+        which to extract the sequences (e.g. 'matrix_analysis_Top_0.XX_Assemblies.tsv')", \
         required=True, type=Path)
-    parser.add_argument("-t", "--targetgenes", help="List of Target Genes", \
+    parser.add_argument("-t", "--targetgenes", help="List of Target Genes (e.g. 'matrix_analysis_S_genes_in_0.XX_of_Top_0.XX_assemblies.tsv')", \
         required=True, type=Path)
     parser.add_argument("-o", "--outputfolder", help="Folder with unaligned sequence file. \
         Default: ./output/Target_Genes_unaligned", \
         type=Path, default="./output/Target_Genes_unaligned")
     parser.add_argument("--aa", help="Extract protein sequences instead of DNA",
         default=False, action="store_true")
-    return parser.parse_args()
+    return parser.parse_args() 
 
 
 def read_list(filepath):
@@ -89,9 +89,23 @@ if __name__ == "__main__":
     target_zips = dict()
     not_found = set()
     for asm in FilteredAssemblies:
-        target_zips[asm] = base_folder / asm / "run_ascomycota_odb10" / "busco_sequences.zip"
-        if not target_zips[asm].is_file():
+        asm_folder = base_folder / asm
+        rfs = list(asm_folder.glob("run_*"))
+        try:
+            assert(len(rfs) == 1)
+        except AssertionError:
+            print(f"Can't find the run folder for assembly {asm}")
+            continue
+        run_folder = rfs[0]
+        
+        # get either the folder or its compressed version
+        if (run_folder / "busco_sequences.zip").is_file():
+            target_zips[asm] = run_folder / "busco_sequences.zip"
+        elif (run_folder / "busco_sequences/single_copy_busco_sequences").is_dir():
+            target_zips[asm] = run_folder / "busco_sequences/single_copy_busco_sequences/"
+        else:
             not_found.add(asm)
+            
     if not_found:
         print("Not found: {} BUSCO 'busco_sequences' zip".format(len(not_found)))
         sys.exit(", ".join(not_found))
@@ -99,18 +113,29 @@ if __name__ == "__main__":
     for gene in TargetGenes:
         with open(o / "{}.{}.fasta".format(gene, file_type), "w") as f:
             for asm in sorted(FilteredAssemblies - not_found):
-                target_zip = target_zips[asm]
+                target = target_zips[asm]
                 
-                with ZipFile(target_zip) as z:
+                if target.is_file():
+                    with ZipFile(target_zip) as z:
+                        try:
+                            with io.TextIOWrapper(z.open("busco_sequences/single_copy_busco_sequences/{}.{}".format(gene, suffix)), encoding="utf-8") as fasta:
+                                old_header = str(fasta.readline())[1:].split(" ")[0]
+                                new_header = ">{}_{} {}".format(gene, asm, old_header)
+                                seq = "".join([str(l).strip() for l in fasta.readlines()])
+                                
+                                f.write("{}\n".format(new_header))
+                                f.write(sequence80(seq))
+                        # this assembly doesn't have a copy of this (S) gene
+                        except KeyError:
+                            f.write(">{}_{}\n".format(gene, asm))
+                else:
                     try:
-                        with io.TextIOWrapper(z.open("busco_sequences/single_copy_busco_sequences/{}.{}".format(gene, suffix)), encoding="utf-8") as fasta:
+                        with open(target / f"{gene}.{suffix}") as fasta:
                             old_header = str(fasta.readline())[1:].split(" ")[0]
-                            new_header = ">{}_{} {}".format(gene, asm, old_header)
+                            new_header = ">{}_{} {}".format(gene, asm, old_header.strip())
                             seq = "".join([str(l).strip() for l in fasta.readlines()])
                             
                             f.write("{}\n".format(new_header))
                             f.write(sequence80(seq))
-                    # this assembly doesn't have a copy of this (S) gene
-                    except KeyError:
+                    except IOError:
                         f.write(">{}_{}\n".format(gene, asm))
-        
